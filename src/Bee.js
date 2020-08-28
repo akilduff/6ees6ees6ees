bf2d2124d72316b6afd24a64d7395ff1d46f5d5e
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-08-27 21:17:42 -0700

    Assignment Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+
+
+  constructor() {
+    super()
+    this.age = 5;
+    this.color = 'yellow'
+    this.job = 'Keep on growing'
+    // console.log(Grub(this));
+
+  }
+    // How to access Grub from here?
+  // console.log(Grub(this))
+
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+
+
+  constructor() {
+    super()
+    this.age = 10;
+    this.color = 'yellow'
+    this.job = 'make honey'
+    this.honeyPot = 0;
+      // console.log(Grub(this));
+
+  }
+
+  makeHoney() {
+    this.honeyPot++
+  }
+
+  giveHoney() {
+    this.honeyPot--
+  }
 };
